120941f986ddca9b83ceca73773042816e02612a
Drew Repasky <drew.repasky@gmail.com>
2012-01-15 15:35:40 -0500

    added web project

---
+++ url-shortener-web/src/main/java/UrlShortenerAction.java
@@ -0,0 +1,5 @@
+public class UrlShortenerAction {
+   public String input() {
+      return "SUCCESS";
+   }
+}
---
+++ url-shortener-web/src/test/java/UrlShortenerActionTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class UrlShortenerActionTest {
+   @Test
+   public void testCanary() {
+      assertTrue(true);
+   }
+}
