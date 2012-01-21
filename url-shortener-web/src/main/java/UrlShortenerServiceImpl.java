ec139353a90932ae77a11f52d32d7129c66f9081
Drew Repasky <drew.repasky@gmail.com>
2012-01-20 20:13:19 -0500

    Added logging using slf4j-api and log4j as the implementation

--- url-shortener-web/src/main/java/UrlShortenerServiceImpl.java
+++ url-shortener-web/src/main/java/UrlShortenerServiceImpl.java
@@ -1,6 +1,12 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class UrlShortenerServiceImpl implements UrlShortenerService {
+
+   private static final Logger logger = LoggerFactory.getLogger(UrlShortenerServiceImpl.class);
+
    public boolean saveUrl(ShortUrl shortUrl) {
-      System.out.println("blah");
+      logger.trace("executing inside UrlShortenerServiceImpl.saveUrl");
       return true;
    }
 }
