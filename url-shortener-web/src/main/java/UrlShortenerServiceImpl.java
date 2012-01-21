67e7c52e80d8ab672140c31989cefd4582914e95
Drew Repasky <drew.repasky@gmail.com>
2012-01-20 20:53:02 -0500

    Added Aspects using Spring that will monitor performance (stop watch) and the trace logging interceptor

--- url-shortener-web/src/main/java/UrlShortenerServiceImpl.java
+++ url-shortener-web/src/main/java/UrlShortenerServiceImpl.java
@@ -6,7 +6,7 @@ public class UrlShortenerServiceImpl implements UrlShortenerService {
    private static final Logger logger = LoggerFactory.getLogger(UrlShortenerServiceImpl.class);
 
    public boolean saveUrl(ShortUrl shortUrl) {
-      logger.trace("executing inside UrlShortenerServiceImpl.saveUrl");
+      logger.error("I still haven't been implemented yet!");
       return true;
    }
 }
